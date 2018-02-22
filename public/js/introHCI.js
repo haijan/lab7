51bb70f65a39b30e8ad8b18c208056b18928837a
Jan Eric De Castro <jedecast.edu>
2018-02-22 03:19:59 -0800

    added viewAlt

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,12 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$('.likeBtn').click(recordWhenLiked);
+}
+
+function recordWhenLiked(event) {
+	event.preventDefault();
+	ga('create', 'UA-114621689-1', 'auto');
+	ga("send", "event", 'like', 'click');
+	console.log("clicked");
 }
